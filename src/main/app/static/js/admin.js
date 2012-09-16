e8ee6ee2bfd47c2807a7f0a19b19fd242ce81596
Xi Shen <davidshen84@gmail.com>
2012-09-16 15:43:24 +0800

    add jslint validation support add yuicompressor support

--- src/main/app/static/js/admin.js
+++ src/main/app/static/js/admin.js
@@ -1,40 +1,48 @@
-$(function() {
+$(function () {
+  'use strict';
+
   // set defaults for underscore.js
   _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g,
     evaluate: /\{\%(.+?)\%\}/g
   };
-  
-  var Blog = Backbone.Model.extend(
-  {
+
+  var Blog, BlogComment, BlogCommentsCollection, BlogEditView,
+    BlogSearchView, BlogCommentsView,
+    commentsView, searchView, editView;
+
+  Blog = Backbone.Model.extend({
     defaults: {
       title: 'empty',
       content: '#empty\n\ncontent',
       tags: [],
       published: false,
       created: null,
-      lastmodified: null },
-    initialize: function() {
-      this.on('change:tags', function() {
+      lastmodified: null
+    },
+    initialize: function () {
+      this.on('change:tags', function () {
         var tags = this.get('tags');
-        if (typeof tags == 'string') {
+        if (typeof tags === 'string') {
           tags = tags.split(',');
           this.set('tags', tags);
-        }});
+        }
+      });
     },
     urlRoot: 'api/sync',
-    publish: function(success) {
+    publish: function (success) {
       this.set('published', !this.get('published'));
       $.ajax({
         type: 'PUT',
         url: this.urlRoot + 'pub/' + encodeURIComponent(this.get('id')),
         data: { published: this.get('published') },
         contentType: 'application/x-www-form-urlencoded',
-        success: success });
+        success: success
+      });
     }
   });
 
-  var BlogComment = Backbone.Model.extend({
+  BlogComment = Backbone.Model.extend({
     defaults: {
       id: '',
       blogtitle: '',
@@ -46,42 +54,46 @@ $(function() {
     urlRoot: 'comment/api/sync'
   });
 
-  var BlogCommentsCollection = Backbone.Collection.extend({
+  BlogCommentsCollection = Backbone.Collection.extend({
     model: BlogComment,
     url: 'comment/api/collection',
-    parse: function(response) {
+    parse: function (response) {
       return response.comments;
     }
   });
 
-  var BlogEditView = Backbone.View.extend({
+  BlogEditView = Backbone.View.extend({
     model: new Blog(),
     searchView: null,
-    template: _.template('<label class=\"text\">Tags: <input type=\"text\" class=\"tags\" value="{{tags}}"/></label>\
-                         <div class=\"btn-group pull-right\">\
-                         <button type=\"button\" class=\"btn btn-primary" {{saved ? \'disabled\' : \'\'}} id=\"btnsave\">Save</button>\
-                         <button type=\"button\" class=\"btn btn-danger\" id=\"btndel\" {{isnew ? \'disabled\' : \'\'}}>Delete</button>\
-                         {% if(!published) { %}\
-                         <button type=\"button\" class=\"btn\" id=\"btnpub\" {{!saved ? \'disabled\' : \'\'}} data-act=\"publish\">Publish</button>\
-                         {% } else { %}\
-                         <button type=\"button\" class=\"btn\" id=\"btnpub\" {{!saved ? \'disabled\' : \'\'}} data-act=\"unpublish\">Unpublish</button>\
-                         {% } %}\
-                         </div>'),
+    template: _.template(
+      '<label class=\"text\">Tags: <input type=\"text\" class=\"tags\" value="{{tags}}"/></label>\
+       <div class=\"btn-group pull-right\">\
+       <button type=\"button\" class=\"btn btn-primary" {{saved ? \'disabled\' : \'\'}} id=\"btnsave\">Save</button>\
+       <button type=\"button\" class=\"btn btn-danger\" id=\"btndel\" {{isnew ? \'disabled\' : \'\'}}>Delete</button>\
+       {% if(!published) { %}\
+       <button type=\"button\" class=\"btn\" id=\"btnpub\" {{!saved ? \'disabled\' : \'\'}} data-act=\"publish\">Publish</button>\
+       {% } else { %}\
+       <button type=\"button\" class=\"btn\" id=\"btnpub\" {{!saved ? \'disabled\' : \'\'}} data-act=\"unpublish\">Unpublish</button>\
+       {% } %}\
+       </div>'
+    ),
     alertTemplate: _.template("<div class='alert alert-{{type}}'>{{msg}}<a class='close' data-dismiss='alert'>x</a></div>"),
-    render: function() {
+    render: function () {
       var view = this;
       // update content view
       this.ee.importFile(view.model.get('title'), view.model.get('content'));
       // update control statuses
       this.$('.controller').html(
-        this.template(_.extend(
-          view.model.toJSON(),
-          {
-            isnew: view.model.isNew(),
-            saved: this['saved'] == true
-          })));
+        this.template(
+          _.extend(view.model.toJSON(),
+            {
+              isnew: view.model.isNew(),
+              saved: this.saved === true
+            })
+        )
+      );
 
-      this.$('.tags').change(function() {
+      this.$('.tags').change(function () {
         var $this = $(this);
         view.model.set('tags', $this.val());
         view.saved = false;
@@ -91,44 +103,44 @@ $(function() {
       return this;
     },
     titlePattern: /^#[^\n\r]*/,
-    extractTitleFromContent: function(content) {
+    extractTitleFromContent: function (content) {
       return this.titlePattern.exec(content);
     },
-    initialize: function() {
-      var view = this;
-      // associate editor view with search view
-      view.options.searchView.editorView = view;
-
+    initialize: function () {
+      var view = this,
       // initialize the EE only once
-      var editor = new EpicEditor({
+        editor = new EpicEditor({
           'container': this.$('.contentview').get(0),
           'basePath': 'static/epiceditor'
-      });
+        });
+
+      // associate editor view with search view
+      view.options.searchView.editorView = view;
       this.ee = editor;
 
       editor.load()
-      .importFile(view.model.get('title'), view.model.get('content'))
-      .preview()
-      // bind save event
-      .on('preview', function() {
-        var content = editor.getElement('editor').body.innerText;
-        view.model.set('content', content);
-        view.saved = false;
-        view.render();
-      });
+        .importFile(view.model.get('title'), view.model.get('content'))
+        //.preview()
+        // bind save event
+        .on('preview', function () {
+          var content = editor.getElement('editor').body.innerText;
+          view.model.set('content', content);
+          view.saved = false;
+          view.render();
+        });
     },
     events: {
       'click #btnsave': 'save',
       'click #btnpub': 'togglePublish',
       'click #btndel': 'destroy'
     },
-    save: function() {
-      var view = this;
-      var content = view.model.get('content');
-      var title = view.extractTitleFromContent(content);
+    save: function () {
+      var view = this,
+        content = view.model.get('content'),
+        title = view.extractTitleFromContent(content);
 
-      if (title == null || title.length == 0) {
-        alert('need title', 'error');
+      if (title === null || title.length === 0) {
+        window.alert('need title', 'error');
         return;
       }
       title = title[0].substr(1);
@@ -136,137 +148,152 @@ $(function() {
       view.model.save(
         { title: title },
         {
-          success: function(model, response) {
+          success: function (model, response) {
             // sync. id with title
             model.set('id', model.get('title'));
-            view['saved'] = true;
+            view.saved = true;
             view.render();
             view.alert('blog saved', 'success');
           }
-        });
+        }
+      );
     },
-    togglePublish: function() {
-      var view = this;
-      var publish = !this.model.get('published');
-      this.model.publish(function(data, textStatus) {
-        if (data.msg == 'ok') {
+    togglePublish: function () {
+      var view = this,
+        publish = !this.model.get('published');
+
+      this.model.publish(function (data, textStatus) {
+        if (data.msg === 'ok') {
           view.render();
           if (publish) {
             view.alert('blog published', 'success');
           } else {
-            view. alert('blog unpublished', 'success');
+            view.alert('blog unpublished', 'success');
           }
         }
       });
     },
-    destroy: function() {
+    destroy: function () {
       var view = this;
       view.model.destroy({
-        success: function(model, response) {
-          if (response.msg == 'ok') {
+        success: function (model, response) {
+          if (response.msg === 'ok') {
             view.model = new Blog();
             view.render();
             view.alert('blog destroied', 'success');
           }
-        }});
+        }
+      });
     },
-    alert: function(msg, type) {
+    alert: function (msg, type) {
       this.$el.prepend(this.alertTemplate({msg: msg, type: type}));
     }
   });
 
-  var BlogSearchView = Backbone.View.extend({
-      // model: new Blogs(),
-      editorView: null,
-      events: {
-        'click .edit': 'edit',
-        'click .new': 'new',
-        'keypress .title': 'updateTypeahead' },
-      updateUI: function() {
-        // update type-ahead data source
-        this.$('input').data('source', this.model.map(function (b) { return b.get('title'); }));
-      },
-      updateTypeahead: function(e) {
-        var that = this;
-        var title = $('.title').val();
-        var working = false;
+  BlogSearchView = Backbone.View.extend({
+    // model: new Blogs(),
+    editorView: null,
+    commentsView: null,
+    events: {
+      'click .edit': 'edit',
+      'click .create': 'create',
+      'keypress .title': 'updateTypeahead'
+    },
+    updateUI: function () {
+      // update type-ahead data source
+      this.$('input').data('source', this.model.map(function (b) { return b.get('title'); }));
+    },
+    updateTypeahead: function (e) {
+      var that = this,
+        title = $('.title').val(),
+        working = false;
 
-        if(title.length > 1 && !working) {
-          working = true;
-          $.get('api/titles?f=' + encodeURIComponent(title),
-            null,
-            function (data, textStatus) {
-              that.$('.title').data('typeahead').source=data.titles;
-              working = false;
-            });
-        }
-      },
-      edit: function() {
-        var view = this.editorView;
-        var title = this.$('input').val();
-        
-        new Blog({id: title})
+      if (title.length > 1 && !working) {
+        working = true;
+        $.get('api/titles?f=' + encodeURIComponent(title),
+          null,
+          function (data, textStatus) {
+            that.$('.title').data('typeahead').source = data.titles;
+            working = false;
+          });
+      }
+    },
+    edit: function () {
+      var view = this.editorView,
+        comments = this.options.commentsView,
+        title = this.$('input').val();
+
+      new Blog({id: title})
         .fetch({
-          success: function(model, response) {
+          success: function (model, response) {
             view.model = model;
             view.saved = true;
             view.render();
 
             // fetch comments
-            commentsView.setBlogTitle(title);
+            comments.setBlogTitle(title);
           }
         });
-      },
-      new: function() {
-        this.editorView.model = new Blog();
-        this.editorView.saved = false;
-        this.editorView.render();
-      }
-    });
+    },
+    create: function () {
+      this.editorView.model = new Blog();
+      this.editorView.saved = false;
+      this.editorView.render();
+    }
+  });
 
-  var BlogCommentsView = Backbone.View.extend({
+  BlogCommentsView = Backbone.View.extend({
     model: new BlogCommentsCollection(),
     blogTitle: '',
-    template: _.template('<ul>{% _.each(comments, function(c) { %}\
-      <li class="comment" id="{{ c.id }}">\
-        <a class="label label-inverse pull-right" href="#">X</a>\
-        <h4>{{ c.screenname }} on {{ c.created }}:</h4>\
-        <p>{{ c.comment }}</p>\
-      </li>\
-      {% }); %}\
-      </ul>'),
-    render: function() {
+    template: _.template(
+      '<ul>{% _.each(comments, function (c) { %}\
+        <li class="comment" id="{{ c.id }}">\
+          <a class="label label-inverse pull-right" href="#">X</a>\
+          <h4>{{ c.screenname }} on {{ c.created }}:</h4>\
+          <p>{{ c.comment }}</p>\
+        </li>\
+        {% }); %}\
+      </ul>'
+    ),
+    render: function () {
       var model = this.model;
 
       this.$el.html(this.template({comments: this.model.toJSON()}));
-      this.$('.comment').bind('close', function(e) {
-        var $this = $(this);
-        if(confirm('really?')) {
-          var id = $this.attr('id');
+      this.$('.comment').bind('close', function (e) {
+        var $this = $(this),
+          id = $this.attr('id');
+        if (window.confirm('really?')) {
           model.get(id).destroy();
           $this.remove();
         }
-      }).children().filter('a').click(function(e) {
+      }).children().filter('a').click(function (e) {
         $(this).parent().trigger('close');
       });
     },
-    setBlogTitle: function(title) {
-      var view = this;
-      var model = this.model;
+    setBlogTitle: function (title) {
+      var view = this,
+        model = this.model;
 
       this.blogTitle = title;
       model.fetch({
         data: {title: this.blogTitle},
-        success: function(collection, response) {
+        success: function (collection, response) {
           view.render();
         }
       });
     }
   });
 
-  var searchView = new BlogSearchView({el: $('#searchview').get(0)});
-  var editView = new BlogEditView({el: $('#viewcontainer').get(0), searchView: searchView}).render();
-  var commentsView = new BlogCommentsView({el: $('#commentsview').get(0)});
+  commentsView = new BlogCommentsView({el: $('#commentsview').get(0)});
+  searchView = new BlogSearchView({
+    el: $('#searchview').get(0),
+    commentsView: commentsView
+  });
+  editView = new BlogEditView({
+    el: $('#viewcontainer').get(0),
+    searchView: searchView
+  })
+    .render();
 
   window.searchView = searchView;
 });
--- src/main/app/static/js/blog.js
+++ src/main/app/static/js/blog.js
@@ -1,34 +1,37 @@
 function redirectLinks() {
+  'use strict';
+
   // redirect links in breadcrumbs
   $('#breadcrumbs > ul a[href]')
-  .click(function(e)
-  {
-    var href = $(this).attr('href');
-    router.navigate(href, {trigger: true});
+    .click(function (e) {
+      var href = $(this).attr('href');
+      window.router.navigate(href, {trigger: true});
 
-    e.preventDefault();
-  });
+      e.preventDefault();
+    });
 
   // redirect links in blog post
   // only relative links should be redirected
   $("#article a[href]")
-  .filter(function() {
-    return !/^http/i.test($(this).attr('href'));
-  })
-  .click(function(e) {
-    router.navigate($(this).attr('href'), {trigger: true});
+    .filter(function () {
+      return !/^http/i.test($(this).attr('href'));
+    })
+    .click(function (e) {
+      window.router.navigate($(this).attr('href'), {trigger: true});
 
-    e.preventDefault();
-  });
+      e.preventDefault();
+    });
 }
 
 // :data: a jQuery object
 // :showComment: control the visibility of the commants and the add comment form
 function updatePage(data, showComment) {
-  var title = data.filter('title').text();
-  var nav = data.filter('#nav');
-  var article = data.filter('article');
-  var comments = data.filter('#comments');
+  'use strict';
+
+  var title = data.filter('title').text(),
+    nav = data.filter('#nav'),
+    article = data.filter('article'),
+    comments = data.filter('#comments');
 
   // update page title
   $('#title').text(title);
@@ -43,8 +46,8 @@ function updatePage(data, showComment) {
 
   // update comments
   $('#comments').html(comments.html());
-  
-  if(showComment) {
+
+  if (showComment) {
     $('#comments').show();
     $('#addcommentform').show();
   } else {
@@ -53,18 +56,23 @@ function updatePage(data, showComment) {
   }
 }
 
-$(function(){
-  var BlogRouter = Backbone.Router.extend({
+$(function () {
+  'use strict';
+
+  var BlogRouter, BlogComment, BlogAddCommentFormView, addComment;
+
+  BlogRouter = Backbone.Router.extend({
     routes: {
-      'about': function() {/*nothing yet!*/},
+      'about': function () {/*nothing yet!*/},
       ':year/:month/:title': 'blog',
       ':year/:month/': 'archives',
       ':year/:month': 'archives',
       ':year/': 'archives',
-      ':year': 'archives' },
-    //initialize: function(options) {/*nothing yet!*/},
-    blog: function(year, month, title) {
-      if(isInitialized) {
+      ':year': 'archives'
+    },
+    //initialize: function (options) {/*nothing yet!*/},
+    blog: function (year, month, title) {
+      if (isInitialized) {
         $.get(year + '/' + month + '/' + encodeURIComponent(title), null,
           function (data, textStatus) {
             updatePage($(data), true);
@@ -75,28 +83,24 @@ $(function(){
         isInitialized = true;
       }
     },
-    archives: function(year, month) {
-      if(isInitialized) {
+    archives: function (year, month) {
+      if (isInitialized) {
         // get archive data
-        var path = year + '/' + (month != undefined ? (month + '/') : '');
+        var path = year + '/' + (month !== undefined ? (month + '/') : '');
 
         $.get(path, null,
-         function (data, textStatus) {
+          function (data, textStatus) {
             updatePage($(data), false);
             redirectLinks();
-         });
+          });
       } else {
         redirectLinks();
         isInitialized = true;
       }
-     }
+    }
   });
 
-  window.router = new BlogRouter();
-  Backbone.history.start({pushState: true, root: refbase});
-
-  var BlogComment = Backbone.Model.extend(
-  {
+  BlogComment = Backbone.Model.extend({
     urlRoot: 'comment/api/sync',
     defaults: {
       blogtitle: '',
@@ -106,15 +110,14 @@ $(function(){
     }
   });
 
-  var BlogAddCommentFormView = Backbone.View.extend(
-  {
+  BlogAddCommentFormView = Backbone.View.extend({
     model: new BlogComment(),
     commentel: $('#comments'),
     commentTemplate: _.template('<dt><em>{{ screenname }}</em> on {{ created }}:</dt><dd class="well">{{ comment }}</dd>'),
     events: {
       'click .clear': 'clear'
     },
-    initialize: function() {
+    initialize: function () {
       var view = this;
 
       this.validator = this.$el.validate({
@@ -131,58 +134,61 @@ $(function(){
             required: true
           }
         },
-        submitHandler: function(form) {
+        submitHandler: function (form) {
           view.save();
         }
       });
     },
-    save: function() {
-      var view = this;
-      var model = this.model;
+    save: function () {
+      var view = this,
+        model = this.model,
+        newComment = null,
+        // update client
+        children = this.commentel.children(),
+        date = new Date(),
+        year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        dateOfMonth = date.getDate() + 1,
+        il = null;
 
       // update to the server
-      var newComment = null;
-      model.save(
-      {
+      model.save({
         'blogtitle': $('#title').attr('keyname'),
         'screenname': this.$('#screenname').val(),
         'email': this.$('#email').val(),
         'comment': this.$('#comment').val()
-      },
-      {
-        error: function(model, response) {
-          if(newComment) {
+      }, {
+        error: function (model, response) {
+          if (newComment) {
             newComment.remove();
           }
         }
       });
 
-      // update client
-      var children = this.commentel.children();
-      var date = new Date();
-      var year = date.getFullYear();
-      var month = date.getMonth() + 1;
+      // create view
       month = month < 10 ? '0' + month : month;
-      var dateOfMonth = date.getDate() + 1;
       dateOfMonth = dateOfMonth > 10 ? dateOfMonth : '0' + dateOfMonth;
-
-      var il = this.commentTemplate(
-      {
-        'email': this.model.get('email'),
-        'screenname': model.get('screenname'),
-        'comment': model.get('comment'),
-        'created': year + '-' + month + '-' + dateOfMonth
-      });
+      il = this.commentTemplate(
+        {
+          'email': this.model.get('email'),
+          'screenname': model.get('screenname'),
+          'comment': model.get('comment'),
+          'created': year + '-' + month + '-' + dateOfMonth
+        }
+      );
 
       this.commentel.append(il);
       newComment = this.commentel.children().last();
     },
-    clear: function() {
+    clear: function () {
       this.$('input').val('');
       this.$('textarea').val('');
       this.validator.resetForm();
     }
   });
 
-  new BlogAddCommentFormView({el: $('#addcommentform form').get(0)});
+  addComment = new BlogAddCommentFormView({el: $('#addcommentform form').get(0)});
+
+  window.router = new BlogRouter();
+  Backbone.history.start({pushState: true, root: refbase});
 });
