b080aadebe08fe57eb68097ed27935c607158767
Xi Shen <davidshen84@gmail.com>
2014-03-28 16:39:50 +0800

    update JS test for admin page

--- src/main/static/admin/js/ng.js
+++ src/main/static/admin/js/ng.js
@@ -11,7 +11,7 @@ angular.module('blogapi', [ 'ngRoute', 'ngResource' ]).
     return $resource('/blog/comment/api/sync/:urlsafe');
   }]);
 
-angular.module('ngapp', ['blogapi'])
+angular.module('ngapp', ['blogapi', 'ngapp.controller'])
   .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/blog/admin/', { "controller": 'ListCtrl', "templateUrl": '/blog/admin/static/bloglist.html' }).
@@ -34,7 +34,6 @@ angular.module('ngapp', ['blogapi'])
       return epiceditor;
     };
   })
-  .controller('RootCtrl', function() {})
   .directive('eeditor', function () {
     return {
       "restrict": 'E',
@@ -57,124 +56,5 @@ angular.module('ngapp', ['blogapi'])
           });
         }}]
     };
-  })
-  .controller('ListCtrl', ['$scope', '$timeout', '$filter', 'Blog', function($scope, $timeout, $filter, Blog) {
-    $scope.blogs = Blog.get();
-
-    $scope.pubIcon = function (published) {
-      return 'glyphicon ' + (published ? 'glyphicon-eye-open' : 'glyphicon-eye-close');
-    };
-
-    $scope.setPubStat = function (index, title, publish) {
-      Blog.update({ "title": encodeURIComponent(title) }, { "published": publish },
-                  function (response) {
-                    if(response.msg == 'ok') {
-                      $timeout(function () {
-                        $scope.$apply('blogs.blogs[' + index + '].published=' + publish);
-                      }, 100);
-                    }
-                  });
-    };
-
-    $scope.getPubAction = function (published) {
-      return published ? 'Unpublish' : 'Publish';
-    };
-
-    $scope.deleteBlog = function (title) {
-      Blog.remove({ "title": encodeURIComponent(title) },
-                  function () {
-                    $timeout(function () {
-                      $scope.$apply(function () {
-                        $scope.blogs.blogs = $filter('filter')($scope.blogs.blogs, {'title': '!' + title});
-                      });
-                    }, 500);
-                  });
-    };
-  }])
-  .controller(
-    'CreateEditCtrl',
-    ['$scope', '$routeParams', '$interpolate', '$sce', 'Blog', 'BlogComment', 'editor',
-     function($scope, $routeParams, $interpolate, $sce, Blog, BlogComment, editor) {
-       function extractTitleFromContent(content) {
-         var match = titlePattern.exec(content);
-
-         return match && match.length > 0 ? match[0].substr(1) : null;
-       }
-
-       var titlePattern = /^#.*$/m,
-           isNew = true,
-           title = $routeParams.title !== undefined ? encodeURIComponent($routeParams.title) : null,
-           notificationTemplate = $interpolate(
-             '<div class="alert alert-{{type}}" data-timestamp={{timestamp}}>\
-               <button type="button" class="close" data-dismiss="alert">&times;</button>\
-             {{msg}}</div>'
-           );
-
-       $scope.isClean = true;
-       $scope.notifyMessage = '';
-
-       if(title) {
-         isNew = false;
-
-         Blog.get({"title": title}, function (blog) {
-           editor().importFile(blog.title, blog.content);
-           $scope.tags = blog.tags.join(', ');
-         });
-
-         // try to get comments
-         $scope.comments = BlogComment.get({ "title": title });
-       }
-
-       $scope.save = function () {
-         var content = editor().exportFile(),
-             title = extractTitleFromContent(content),
-             tags = $scope.tags || '';
-
-         if(title === null) {
-           window.alert('blog needs a title');
-           return;
-         }
-
-         function updateSuccess(data) {
-           $scope.isClean = true;
-           isNew = false;
-           $scope.notifyMessage = $sce.trustAsHtml(notificationTemplate({
-             "msg": data.msg,
-             "type": "success",
-             "timestamp": +new Date()
-           }));
-         }
-
-         if(isNew) {
-           Blog.save(
-             { "title": title,
-               "content": content,
-               "tags": tags.length ? $scope.tags.split(',') : [] },
-             updateSuccess
-           );
-         } else {
-           Blog.update(
-             { "title": title },
-             { "content": content,
-               "tags": tags.length ? $scope.tags.split(',') : [] },
-             updateSuccess
-           );
-         }
-       };
-
-       $scope.deleteComment = function (id) {
-         BlogComment.remove({ "title": id }, null,
-                            function () {
-                              $scope.comments = BlogComment.get({ "title": title });
-                            });
-       };
-
-       $scope.showMsg = function () {
-         if($scope.lastAction) {
-           return notificationTemplate($scope.lastAction);
-         }
-
-         return null;
-       };
-     }]);
+  });
 
---
+++ src/test/clientside/specs/adminSpecs.js
@@ -0,0 +1,217 @@
+describe('Blog module', function() {
+  'use strict';
+
+  var $http;
+
+  beforeEach(module('ngapp'));
+  beforeEach(inject(function ($httpBackend) {
+    $http = $httpBackend;
+  }));
+
+  afterEach(function(){
+    $http.verifyNoOutstandingExpectation();
+    $http.verifyNoOutstandingRequest();
+  });
+
+  it('should resolve Blog', inject(function(Blog) {
+    Blog.should.not.equal(null);
+    Blog.should.not.equal(undefined);
+  }));
+
+  it('should make GET request to /blog/api/sync', inject(function (Blog) {
+    $http.when('GET', '/blog/api/sync', undefined).respond('');
+    $http.expect('GET', '/blog/api/sync', undefined);
+    Blog.get();
+
+    $http.flush();
+  }));
+
+  it('should make PUT request to /blog/api/sync/test', inject(function(Blog) {
+    $http.when('PUT', '/blog/api/sync/test').respond(null);
+    $http.expect('PUT', '/blog/api/sync/test', {});
+    Blog.update({ "title": "test"}, {});
+
+    $http.flush();
+  }));
+
+  it('should make POST request to /blog/api/sync/test', inject(function(Blog) {
+    var data = {
+      "title": 'test',
+      "content": 'content',
+      "tags": []
+    };
+
+    $http.when('POST', '/blog/api/sync').respond(null);
+    $http.expect('POST', '/blog/api/sync', data);
+    Blog.save(data);
+
+    $http.flush();
+  }));
+});
+
+describe('eeditor directive', function() {
+  'use strict';
+
+  beforeEach(module('ngapp'));
+
+  it('should resolve editor factory', inject(function (editor) {
+    editor.should.not.equal(null);
+    editor.should.not.equal(undefined);
+    editor.should.be.a('function');
+  }));
+});
+
+describe('BlogComment module', function() {
+  'use strict';
+
+  var $http;
+
+  beforeEach(module('ngapp'));
+  beforeEach(inject(function($httpBackend) {
+    $http = $httpBackend;
+  }));
+
+  afterEach(function(){
+    $http.verifyNoOutstandingExpectation();
+    $http.verifyNoOutstandingRequest();
+  });
+
+  it('should resolve BlogComment', inject(function(BlogComment) {
+    BlogComment.should.not.equal(null);
+    BlogComment.should.not.equal(undefined);
+  }));
+
+  it('should make DELETE request to /blog/comment/api/sync', inject(function(BlogComment) {
+    var data = {
+      "title": 1
+    };
+
+    $http.when('DELETE', '/blog/comment/api/sync/1', '').respond(null);
+    $http.expect('DELETE', '/blog/comment/api/sync/1');
+
+    BlogComment.remove(data);
+    $http.flush();
+  }));
+});
+
+describe('ListCtrl', function () {
+  'use strict';
+
+  var ctrl, scope;
+
+  beforeEach(module('ngapp'));
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+    ctrl = $controller;
+    scope = $rootScope.$new();
+  }));
+
+  it('should call Blog.get on initialize', inject(function(Blog) {
+    var spy = sinon.spy(),
+        stub = sinon.stub(Blog, 'get', spy);
+
+    ctrl('ListCtrl', { "$scope": scope, "Blog": Blog });
+    spy.called.should.be.equal(true);
+  }));
+
+  it('should return correct published icon', inject(function (Blog) {
+    sinon.stub(Blog, 'get', null);
+    ctrl('ListCtrl', { "$scope": scope, "Blog": Blog });
+
+    scope.pubIcon(true).should.be.equal('glyphicon glyphicon-eye-open');
+    scope.pubIcon(false).should.be.equal('glyphicon glyphicon-eye-close');
+  }));
+
+  it('should call Blog.update with publish=true', inject(function (Blog) {
+    var updateSpy = sinon.spy();
+
+    sinon.stub(Blog, 'get', null);
+    sinon.stub(Blog, 'update', updateSpy);
+
+    ctrl('ListCtrl', { "$scope": scope, "Blog": Blog });
+    scope.setPubStat(0, 'test', true);
+    updateSpy.calledWith(
+      { "title": 'test' },
+      { "published": true },
+      sinon.match.func).should.be.ok;
+  }));
+
+  it('should call Blog.remove with correct parameters', inject(function (Blog) {
+    var removeSpy = sinon.spy();
+
+    sinon.stub(Blog, 'get', null);
+    sinon.stub(Blog, 'remove', removeSpy);
+
+    ctrl('ListCtrl', { "$scope": scope, "Blog": Blog });
+
+    scope.deleteBlog('test');
+    removeSpy.calledWith({"title": 'test'}).should.be.ok;
+  }));
+});
+
+describe('CreateEditCtrl', function() {
+  'use strict';
+
+  var ctrl, scope, http;
+
+  beforeEach(module('ngapp'));
+  beforeEach(inject(function($controller, $rootScope, $httpBackend) {
+    ctrl = $controller;
+    scope = $rootScope.$new();
+    http = $httpBackend;
+  }));
+
+  it('should call Blog.get and BlogComment.get when title is set', inject(function(Blog, BlogComment, editor) {
+    var spyBlogGet = sinon.spy(Blog, 'get'),
+        spyBlogCommentGet = sinon.spy(BlogComment, 'get'),
+        routeParams = {"title": 'test'};
+
+    ctrl('CreateEditCtrl',
+         {"$scope": scope, "$routeParams": routeParams, "Blog": Blog,
+          "BlogComment": BlogComment, "editor": editor});
+    
+    spyBlogGet.should.have.been.calledWith(routeParams);
+    spyBlogCommentGet.should.have.been.calledWith(routeParams);
+  }));
+
+  it('should import data to editor', inject(function(Blog, BlogComment) {
+    var routeParams = {"title": 'test'},
+        editor = sinon.stub(),
+        spyImportFile = sinon.spy();
+
+    http.when('GET', '/blog/api/sync/test').respond({
+      "title": 'test respond',
+      "content": 'content',
+      "tags": ['1', '2']
+    });
+    http.when('GET', '/blog/comment/api/sync/test').respond(null);
+
+    editor.returns({
+      "importFile": spyImportFile
+    });
+    ctrl('CreateEditCtrl',
+         {"$scope": scope, "$routeParams": routeParams, "Blog": Blog,
+          "BlogComment": BlogComment, "editor": editor});
+
+    http.flush();
+    spyImportFile.calledWith('test respond', 'content').should.be.ok;
+    scope.tags.should.be.equal('1, 2');
+  }));
+});
+
+// i have no idea...
+describe.skip('ng route', function () {
+  beforeEach(module('ngapp'));
+
+  it('should route to /blog/admin', inject(function ($route, $location, $browser) {
+    var routeObj = $route.routes['/blog/admin'],
+        spyCtrl = sinon.spy();
+
+    routeObj.should.not.be.equal(undefined);
+    routeObj.controller = spyCtrl;
+    $location.path('/blog/admin/');
+
+    spyCtrl.called.should.be.ok;
+
+  }));
+});
+
