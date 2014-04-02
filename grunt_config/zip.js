2ec27f58550552c84eeee43b6b6be6f72bed6896
Xi Shen <davidshen84@gmail.com>
2014-04-02 19:15:09 +0800

    add load-grunt-config and split gruntfile.js

---
+++ grunt_config/zip.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "werkzeug": {
+    "cwd": '/usr/lib/python2.7/dist-packages/',
+    "src": [ '/usr/lib/python2.7/dist-packages/werkzeug/*.py',
+             '/usr/lib/python2.7/dist-packages/werkzeug/contrib/*.py' ],
+    "dest": 'build/werkzeug.zip'
+  },
+  "flask": {
+    "cwd": '/usr/lib/python2.7/dist-packages/',
+    "src": '/usr/lib/python2.7/dist-packages/flask/**/*.py',
+    "dest": "build/flask.zip"
+  },
+  "bloglib": {
+    "cwd": "bloglib/src/",
+    "src": "bloglib/src/bloglib/*.py",
+    "dest": "lib/bloglib.zip"
+  }
+};
