e55d27358bd9dde3b7730ab5357e2c380f2a80e8
Xi <davidshen84@hotmail.com>
2015-09-13 10:52:03 +0800

    hide some js error; fix copy path

--- grunt_config/copy.js
+++ grunt_config/copy.js
@@ -3,19 +3,19 @@ module.exports = {
     "expand": true,
     "cwd": 'bower_components/epiceditor/epiceditor',
     "src": '**',
-    "dest": 'src/main/app/static/epiceditor'
+    "dest": 'app/static/epiceditor'
   },
   "angular-ui": {
     "expand": true,
     "cwd": 'bower_components/angular-ui-bootstrap/dist/',
     "src": [ '*.min.js' ],
-    "dest": 'src/main/app/static/angular-ui/js'
+    "dest": 'app/static/angular-ui/js'
   },
   "angular-ui-template": {
     "expand": true,
     "cwd": 'bower_components/angular-ui-bootstrap/template',
-    "src": [ 'accordion/*.js' ],
-    "dest": 'src/main/app/static/angular-ui/template'
+    "src": [ 'accordion' ],
+    "dest": 'app/static/angular-ui/template'
   }
 };
 
