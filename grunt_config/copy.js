cd44f5b084610edefcc0a7c44b02cab0820b8b97
Xi Shen <davidshen84@gmail.com>
2015-10-06 15:16:34 +0800

    move js libs to libs change comment avatar image

--- grunt_config/copy.js
+++ grunt_config/copy.js
@@ -3,19 +3,19 @@ module.exports = {
     "expand": true,
     "cwd": 'bower_components/epiceditor/epiceditor',
     "src": '**',
-    "dest": 'app/static/epiceditor'
+    "dest": 'app/static/libs/epiceditor'
   },
   angular: {
     expand: true,
     cwd: 'bower_components/angular-latest/build',
     src: ['angular.min.js', 'angular-resource.min.js', 'angular-route.min.js'],
-    dest: 'app/static/angular'
+    dest: 'app/static/libs/angular'
   },
   material: {
     expand: true,
     cwd: 'bower_components/material-design-lite',
     src: [ 'material.min.js', 'material.min.css' ],
-    dest: 'app/static/material'
+    dest: 'app/static/libs/material'
   }
 };
 
