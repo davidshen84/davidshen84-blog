00345dc3c413f14922b35e2139f567dc9484b469
Xi Shen <davidshen84@gmail.com>
2016-09-10 18:40:44 +0800

    Update controller definition

--- app/blog/static/blog/js/blog.js
+++ app/blog/static/blog/js/blog.js
@@ -4,29 +4,28 @@
   angular.module('blog', ['blogapi'])
     .component('xsComment', {
       templateUrl: 'blog/static/blog/comment-template.html',
-      controller: 'CommentCtrl'
-    })
-    .controller('CommentCtrl', ['$scope', '$location', 'BlogComment', function ($scope, $location, Comment) {
-      componentHandler.upgradeAllRegistered();
+      controller: ['$scope', '$location', 'BlogComment', function ($scope, $location, Comment) {
+        componentHandler.upgradeAllRegistered();
 
-      var default_comment = {};
+        var default_comment = {};
 
-      // build params from url
-      var urlSplits = $location.absUrl().split('/');
-      var params = {urlsafe: urlSplits[urlSplits.length - 1]};
+        // build params from url
+        var urlSplits = $location.absUrl().split('/');
+        var params = {urlsafe: urlSplits[urlSplits.length - 1]};
 
-      $scope.comment = new Comment(angular.copy(default_comment));
+        $scope.comment = new Comment(angular.copy(default_comment));
 
-      // get comments for the given blog
-      $scope.comments = Comment.query(params);
+        // get comments for the given blog
+        $scope.comments = Comment.query(params);
 
-      // save comment
-      $scope.submit = function () {
-        $scope.comment.$save(params, function (comment) {
-          $scope.comments.unshift(comment);
-          $scope.comment = new Comment(angular.copy(default_comment));
-        });
-      };
-    }]);
+        // save comment
+        $scope.submit = function () {
+          $scope.comment.$save(params, function (comment) {
+            $scope.comments.unshift(comment);
+            $scope.comment = new Comment(angular.copy(default_comment));
+          });
+        };
+      }]
+    });
 })(angular, componentHandler);
 
