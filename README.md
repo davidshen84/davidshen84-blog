9624ab9a80bca29b80b9fec80ec057294ecaace3
David Shen <davidshen84@gmail.com>
2018-10-18 21:05:42 +0800

    Update README.md.

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ My blog on GAE
 Features:
 
     - Flask
+    - Flask REST-ful
     - Gulp
--- app/lib/__init__.py
+++ app/lib/__init__.py
@@ -4,7 +4,6 @@ __author__ = 'david'
 
 requirements = """
 Flask==0.10.1
-Markdown==2.6.6
 Flask_RESTful=0.3.5
 marshmallow==2.15.6
 """
